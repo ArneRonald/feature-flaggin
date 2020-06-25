94c560b8f643199860bd356c1c94458c4d72d783
Arne <arneengelbrecht27@gmail.com>
2020-06-25 20:32:38 +0200

    Initial commit

---
+++ src/main/java/za/co/feature/flagin/featureflags/FeatureFlagsApplication.java
@@ -0,0 +1,13 @@
+package za.co.feature.flagin.featureflags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/feature/flagin/featureflags/FeatureFlagsApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.feature.flagin.featureflags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
